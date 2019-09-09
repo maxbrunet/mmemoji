030604a77c63b0294b7c0d5a37c6bccfdc92a803
Maxime Brunet <max@brnt.mx>
2019-09-08 23:58:53 -0400

    Make test suite less flaky in case of failure
    By ensuring the proper emojis exist (or don't) using the reconcilier
    pattern

--- tests/test_commands_create.py
+++ tests/test_commands_create.py
@@ -4,13 +4,7 @@ from click.testing import CliRunner
 
 from mmemoji.cli import cli
 
-from .utils import (
-    EMOJIS,
-    create_emojis,
-    delete_emojis,
-    find_dict_in_list,
-    user_env,
-)
+from .utils import EMOJIS, emoji_inventory, find_dict_in_list, user_env
 
 
 def test_help():
@@ -25,15 +19,13 @@ def test_create_emoji(cli_runner):
     emoji_path = EMOJIS[emoji_name]
     user = "user-1"
     # Test
-    with user_env(user):
+    with user_env(user), emoji_inventory([], user):
         result = cli_runner.invoke(cli, ["create", emoji_path, "-o", "json"])
     emoji_list = json.loads(result.stdout)
     emoji = find_dict_in_list(emoji_list, "name", emoji_name)
     assert result.exit_code == 0
     assert len(emoji_list) == 1
     assert emoji["name"] == emoji_name
-    # Teardown
-    delete_emojis([emoji_name], user)
 
 
 def test_create_exiting_emoji(cli_runner):
@@ -41,15 +33,12 @@ def test_create_exiting_emoji(cli_runner):
     emoji_name = "emoji_1"
     emoji_path = EMOJIS[emoji_name]
     user = "user-1"
-    create_emojis([emoji_name], user)
     # Test
-    with user_env(user):
+    with user_env(user), emoji_inventory([emoji_name], user):
         result = cli_runner.invoke(cli, ["create", emoji_path, "-o", "json"])
     assert result.exit_code == 1
     error = result.stderr.split("\n")[-2]
     assert error == 'Error: Emoji "{}" exists'.format(emoji_name)
-    # Teardown
-    delete_emojis([emoji_name], user)
 
 
 def test_force_create_emoji(cli_runner):
@@ -58,7 +47,7 @@ def test_force_create_emoji(cli_runner):
     emoji_path = EMOJIS[emoji_name]
     user = "user-1"
     # Test
-    with user_env(user):
+    with user_env(user), emoji_inventory([], user):
         result = cli_runner.invoke(
             cli, ["create", "--force", emoji_path, "-o", "json"]
         )
@@ -67,8 +56,6 @@ def test_force_create_emoji(cli_runner):
     assert result.exit_code == 0
     assert len(emoji_list) == 1
     assert emoji["name"] == emoji_name
-    # Teardown
-    delete_emojis([emoji_name], user)
 
 
 def test_force_create_existing_emoji(cli_runner):
@@ -76,9 +63,8 @@ def test_force_create_existing_emoji(cli_runner):
     emoji_name = "emoji_1"
     emoji_path = EMOJIS[emoji_name]
     user = "user-1"
-    create_emojis([emoji_name], user)
     # Test
-    with user_env(user):
+    with user_env(user), emoji_inventory([emoji_name], user):
         result = cli_runner.invoke(
             cli, ["create", "--force", emoji_path, "-o", "json"]
         )
@@ -87,8 +73,6 @@ def test_force_create_existing_emoji(cli_runner):
     assert result.exit_code == 0
     assert len(emoji_list) == 1
     assert emoji["name"] == emoji_name
-    # Teardown
-    delete_emojis([emoji_name], user)
 
 
 def test_no_clobber_create_emoji(cli_runner):
@@ -97,7 +81,7 @@ def test_no_clobber_create_emoji(cli_runner):
     emoji_path = EMOJIS[emoji_name]
     user = "user-1"
     # Test
-    with user_env(user):
+    with user_env(user), emoji_inventory([], user):
         result = cli_runner.invoke(
             cli, ["create", "--no-clobber", emoji_path, "-o", "json"]
         )
@@ -106,8 +90,6 @@ def test_no_clobber_create_emoji(cli_runner):
     assert result.exit_code == 0
     assert len(emoji_list) == 1
     assert emoji["name"] == emoji_name
-    # Teardown
-    delete_emojis([emoji_name], user)
 
 
 def test_no_clobber_create_existing_emoji(cli_runner):
@@ -115,16 +97,13 @@ def test_no_clobber_create_existing_emoji(cli_runner):
     emoji_name = "emoji_1"
     emoji_path = EMOJIS[emoji_name]
     user = "user-1"
-    create_emojis([emoji_name], user)
     # Test
-    with user_env(user):
+    with user_env(user), emoji_inventory([emoji_name], user):
         result = cli_runner.invoke(
             cli, ["create", "--no-clobber", emoji_path, "-o", "json"]
         )
     assert result.exit_code == 0
     assert result.stdout == ""
-    # Teardown
-    delete_emojis([emoji_name], user)
 
 
 def test_interactive_create_emoji(cli_runner):
@@ -134,12 +113,9 @@ def test_interactive_create_emoji(cli_runner):
     # 3rd will exist and will not be overwritten
     user = "user-1"
     emoji_names = ["emoji_1", "emoji_2", "emoji_3"]
-    emoji_paths = []
-    for name in emoji_names:
-        emoji_paths.append(EMOJIS[name])
-    create_emojis(emoji_names[1:], user)
+    emoji_paths = [EMOJIS[name] for name in emoji_names]
     # Test
-    with user_env(user):
+    with user_env(user), emoji_inventory(emoji_names[1:], user):
         result = cli_runner.invoke(
             cli,
             ["create", "--interactive", "-o", "json"] + emoji_paths,
@@ -153,5 +129,3 @@ def test_interactive_create_emoji(cli_runner):
     assert len(emoji_list) == 2
     assert emoji1["name"] == emoji_names[0]
     assert emoji2["name"] == emoji_names[1]
-    # Teardown
-    delete_emojis(emoji_names, user)
