db24364c5ed41a0f50e9501a84fd68de8ce65741
Maxime Brunet <max@brnt.mx>
2022-10-30 17:34:48 -0700

    chore: update sampledata generation to use mmctl (#205)

--- tests/scripts/generate-sampledata.sh
+++ tests/scripts/generate-sampledata.sh
@@ -3,8 +3,19 @@ set -euo pipefail
 
 CONTAINER="${MATTERMOST_CONTAINER:-mattermost-mmemoji}"
 
-docker exec -i "${CONTAINER}" bash <<EOF
-  mattermost sampledata --bulk /sampledata.json \
+docker exec -i "${CONTAINER}" bash -e <<EOF
+  # Chicken or the egg problem: It is required to be logged in
+  # to generate sample data, even with --bulk
+  if ! mmctl auth current >/dev/null 2>&1; then
+    mmctl auth login http://localhost:8065 \
+      --name local-server \
+      --username sysadmin \
+      --password-file /dev/stdin \
+      >/dev/null <<<'Sys@dmin-sample1'
+  fi
+
+  mmctl sampledata \
+    --bulk /dev/stdout \
     --channel-memberships 0 \
     --channels-per-team 0 \
     --direct-channels 0 \
@@ -15,6 +26,5 @@ docker exec -i "${CONTAINER}" bash <<EOF
     --posts-per-group-channel 0 \
     --team-memberships 1 \
     --teams 1 \
-    --users 2 1>&2
-  cat /sampledata.json
+    --users 2
 EOF
